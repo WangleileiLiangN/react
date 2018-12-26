e9627a06b1a95e0152201058d78a6d5d488ca62b
wangleilei <247365731@qq.com>
2018-12-26 22:23:45 +0800

    rea

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import $ from 'axios'
+class Marry extends React.Component{
+	render(){
+		return(
+			<div>nihao</div>
+		)
+	}
+
+}
+ReactDOM.render(
+	<Marry/>,
+	document.getElementById("app")
+)
+console.log('ok')
